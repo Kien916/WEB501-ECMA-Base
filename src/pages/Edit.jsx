1e989f600d78e53d8d208b1408f0ab50f342b7b1
Kien916 <kientrungkien133@gmail.com>
2025-12-03 14:16:23 +0700

    edit

--- src/pages/Edit.jsx
+++ src/pages/Edit.jsx
@@ -1,5 +1,12 @@
-
+import {useState} from 'react'
+import {toast} from 'react-hot-toast'
+import axios from 'axios'
 function Edit() {
+  const [name, setName] = useState('')
+  const [price, setPrice] = useState('')
+  const [category, setCategory] = useState('Tour ngoại quốc')
+
+  
   return (
     <div>
       <h1>Cập nhật</h1>
